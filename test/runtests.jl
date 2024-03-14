f4aa643202b5a2dc708b67605eacbe34c22fab2e
SivaVisves <siva18visu@gmail.com>
2024-03-14 15:20:01 -0400

    Updated runtest.jl to relative directory

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,9 +14,9 @@ using CSV, DataFrames, Plots, StatsPlots, Distributions, Random, KernelDensity, 
     load_scenario_test = load_scenarios(file_load, "DateTimeTexas")
 
     # load historical quantiles
-    df_wind = CSV.read("Historical Quantiles/df_wind_2018_historical_quantiles.csv", DataFrame);
-    df_solar = CSV.read("Historical Quantiles/df_solar_2018_historical_quantiles.csv", DataFrame);
-    df_load = CSV.read("Historical Quantiles/df_load_2018_historical_quantiles.csv", DataFrame);
+    df_wind = CSV.read("../Historical Quantiles/df_wind_2018_historical_quantiles.csv", DataFrame);
+    df_solar = CSV.read("../Historical Quantiles/df_solar_2018_historical_quantiles.csv", DataFrame);
+    df_load = CSV.read("../Historical Quantiles/df_load_2018_historical_quantiles.csv", DataFrame);
     
     # correction DateTimeTexas
     df_wind.DateTimeTexas = df_wind.DateTime .- Hour(6);
@@ -29,9 +29,9 @@ using CSV, DataFrames, Plots, StatsPlots, Distributions, Random, KernelDensity, 
     df_load.extracted_hour = hour.(df_load.DateTimeTexas);
 
     # load quantile data
-    wind_event_quantile = CSV.read("Quantiles/Wind Quantiles.csv", DataFrame);
-    solar_event_quantile = CSV.read("Quantiles/Solar Quantiles.csv", DataFrame);
-    load_event_quantile = CSV.read("Quantiles/Load Quantiles.csv", DataFrame);
+    wind_event_quantile = CSV.read("../Quantiles/Wind Quantiles.csv", DataFrame);
+    solar_event_quantile = CSV.read("../Quantiles/Solar Quantiles.csv", DataFrame);
+    load_event_quantile = CSV.read("../Quantiles/Load Quantiles.csv", DataFrame);
 
     hour_of_interest = 0;
     horizon = 47;
@@ -44,13 +44,13 @@ using CSV, DataFrames, Plots, StatsPlots, Distributions, Random, KernelDensity, 
     t = scenario_generation(df_wind, df_solar, df_load, wind_event_quantile, solar_event_quantile, load_event_quantile, month_of_interest, day_of_interest, horizon, hour_of_interest, k)
 
 
-    data_to_hdf5("test/wind_scenario_blocks_hour_$hour_of_interest", wind_scenario_blocks_final_variance1)
-    data_to_hdf5("test/solar_scenario_blocks_hour_$hour_of_interest", solar_scenario_blocks_final_variance1)
-    data_to_hdf5("test/load_scenario_blocks_hour_$hour_of_interest", load_scenario_blocks_final_variance1)
+    data_to_hdf5("../test/wind_scenario_blocks_hour_$hour_of_interest", wind_scenario_blocks_final_variance1)
+    data_to_hdf5("../test/solar_scenario_blocks_hour_$hour_of_interest", solar_scenario_blocks_final_variance1)
+    data_to_hdf5("../test/load_scenario_blocks_hour_$hour_of_interest", load_scenario_blocks_final_variance1)
 
-    file_wind = "test/wind_scenario_blocks_hour_0.h5"
-    file_solar = "test/solar_scenario_blocks_hour_0.h5"
-    file_load = "test/load_scenario_blocks_hour_0.h5"
+    file_wind = "../test/wind_scenario_blocks_hour_0.h5"
+    file_solar = "../test/solar_scenario_blocks_hour_0.h5"
+    file_load = "../test/load_scenario_blocks_hour_0.h5"
 
     @test wind_scenario = load_scenarios(file_wind, "DateTimeTexas") == wind_scenario_test
     @test solar_scenario = load_scenarios(file_solar, "DateTimeTexas") == solar_scenario_test
