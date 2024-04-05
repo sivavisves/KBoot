d7f01c28affc9ad744a1f9abe0c9646abec5252b
Han-shu <1051565276@qq.com>
2024-04-05 16:54:19 -0400

    new file:   data_process.jl 	new file:   run.jl

---
+++ data_process.jl
@@ -0,0 +1,116 @@
+using StatsBase, CSV, DataFrames, Plots, StatsPlots, Distributions, Random, KernelDensity, Dates, NearestNeighbors, Statistics, TimeZones
+using HDF5
+gr(size=(600, 600))
+include("src/quantile_conversion.jl")
+
+# Funtions to read arpa-e-perform h5 files into DataFrame
+function read_actuals(filename::AbstractString; isload = false)::DataFrame
+    df = DataFrame(DateTime = DateTime[], hour = Int64[], BA_total = Float64[])
+    data = h5open(filename, "r") do file
+        return read(file, "actuals")
+    end
+    init_time = DateTime(2019, 1, 1, 0, 0, 0)
+    for i in 1:8760
+        curr_time = init_time + Hour(i - 1)
+        hour = (i-1)%24
+        if isload
+            val = mean(data[8760+i])
+        else
+            val = mean(data[(i-1)*12+1:i*12])
+        end
+        push!(df, [curr_time hour val])
+    end
+    return df
+end
+
+function  read_forecast_quantiles(filename::AbstractString; isload = false)::DataFrame
+    forecast = h5open(filename, "r") do file
+        return read(file)
+    end
+    header = ["x$i" for i in 1:99]
+    date_time_format = "yyyy-mm-dd HH:MM:SS+zzzz"
+    forecast_times = [DateTime(dt_str, date_time_format) for dt_str in forecast["forecast_time"]]
+    issue_times = [DateTime(dt_str, date_time_format) for dt_str in forecast["issue_time"]]
+    if isload
+        ncols = size(forecast["forecasts"], 2)
+        new_matrix = zeros(Float32, size(forecast["forecasts"], 1), Int(ncols/2))
+        for col in 1:2:ncols
+            new_matrix[:, cld(col, 2)] = mean(forecast["forecasts"][:, col:col+1], dims=2)
+        end
+        df = DataFrame(transpose(new_matrix), header)
+        df.datetime = [forecast_times[2*i] for i in 1:8760]
+        df.datetime_issue = [issue_times[2*i] for i in 1:8760]
+    else
+        qunatile_matrix = [parse(Float32, element) for element in forecast["forecasts"]]
+        df = DataFrame(transpose(qunatile_matrix), header)
+        df.datetime = forecast_times
+        df.datetime_issue = issue_times
+    end
+    return df
+end
+
+function  read_forecast_8760(filename::AbstractString; isload = false)::DataFrame
+    forecast = h5open(filename, "r") do file
+        return read(file)
+    end
+    header = ["h$i" for i in 1:8760]
+    if isload
+        ncols = size(forecast["forecasts"], 2)
+        new_matrix = zeros(Float32, size(forecast["forecasts"], 1), Int(ncols/2))
+        for col in 1:2:ncols
+            new_matrix[:, cld(col, 2)] = mean(forecast["forecasts"][:, col:col+1], dims=2)
+        end
+        df = DataFrame(new_matrix, header)
+    else
+        qunatile_matrix = [parse(Float32, element) for element in forecast["forecasts"]]
+        df = DataFrame(qunatile_matrix, header)
+    end
+    return df
+end
+
+
+
+
+data_dir = "/Users/hanshu/Desktop/Price_formation/Data/ARPAE_NYISO"
+output_dir = "/Users/hanshu/Desktop/Price_formation/Data/generate_fr_KBoot/idx_hour/"
+
+#---------------------------Data Loading---------------------------#
+solar_fcst_file = joinpath(data_dir, "BA_solar_1day-ahead_fcst_2019.h5")
+wind_fcst_file = joinpath(data_dir, "BA_wind_1day-ahead_fcst_2019.h5")
+load_fcst_file = joinpath(data_dir, "BA_load_1day-ahead_fcst_2019.h5")
+solar_actual_file = joinpath(data_dir, "BA_solar_actuals_2019.h5")
+wind_actual_file = joinpath(data_dir, "BA_wind_actuals_2019.h5")
+load_actual_file = joinpath(data_dir, "BA_load_actuals_2019.h5")
+
+df_wind = read_actuals(wind_actual_file)
+df_solar = read_actuals(solar_actual_file)
+df_load = read_actuals(load_actual_file; isload = true)
+
+df_wind_forecast_quantiles = read_forecast_quantiles(wind_fcst_file)
+df_solar_forecast_quantiles = read_forecast_quantiles(solar_fcst_file)
+df_load_forecast_quantiles = read_forecast_quantiles(load_fcst_file; isload = true)
+
+
+#---------------------------Preprocessing---------------------------#
+x = df_wind_forecast_quantiles.datetime .- Hour(6);
+
+df_wind_forecast_quantiles = insertcols!(df_wind_forecast_quantiles, 1, :DateTimeTexas => x);
+df_solar_forecast_quantiles = insertcols!(df_solar_forecast_quantiles, 1, :DateTimeTexas => x);
+df_load_forecast_quantiles = insertcols!(df_load_forecast_quantiles, 1, :DateTimeTexas => x);
+df_wind = insertcols!(df_wind, 1, :DateTimeTexas => x);
+df_solar = insertcols!(df_solar, 1, :DateTimeTexas => x);
+df_load = insertcols!(df_load, 1, :DateTimeTexas => x);
+
+# only take DateTime at year 2017-12-31
+df_wind = df_wind[df_wind.DateTime .>= DateTime(2019, 01, 01, 0, 0, 0), :];
+df_solar = df_solar[df_solar.DateTime .>= DateTime(2019, 01, 01, 0, 0, 0), :];
+df_load = df_load[df_load.DateTime .>= DateTime(2019, 01, 01, 0, 0, 0), :];
+
+#---------------------------Function---------------------------#
+# Load quantiles for conversion
+df_wind_forecast = read_forecast_8760(wind_fcst_file)
+df_solar_forecast = read_forecast_8760(solar_fcst_file)
+df_load_forecast = read_forecast_8760(load_fcst_file; isload = true)
+
+# x = process_forecast_quantiles(df_wind, df_solar, df_load, df_wind_forecast_quantiles, df_solar_forecast_quantiles, df_load_forecast_quantiles, df_wind_forecast, df_solar_forecast, df_load_forecast);
+
---
+++ run.jl
@@ -0,0 +1,78 @@
+using KBoot
+using HDF5
+using CSV, DataFrames, Plots, StatsPlots, Distributions, Random, KernelDensity, Dates, NearestNeighbors, Statistics, TimeZones, HDF5
+
+function covert2array(vec_df::Vector{Any})::Array{Float64, 2}
+    array = zeros(Float64, length(vec_df[1].BA_total), length(vec_df))
+    for (i, df) in enumerate(vec_df)
+        array[:, i] = df.BA_total
+    end
+    return array
+end
+
+# # load historical quantiles
+# df_wind = CSV.read("Historical Quantiles/df_wind_2018_historical_quantiles.csv", DataFrame);
+# df_solar = CSV.read("Historical Quantiles/df_solar_2018_historical_quantiles.csv", DataFrame);
+# df_load = CSV.read("Historical Quantiles/df_load_2018_historical_quantiles.csv", DataFrame);
+
+function  read_event_quantiles(filename::AbstractString; isload = false)::DataFrame
+    forecast = h5open(filename, "r") do file
+        return read(file)
+    end
+    header = ["p_$i" for i in 1:99]
+    if isload
+        ncols = size(forecast["forecasts"], 2)
+        new_matrix = zeros(Float32, size(forecast["forecasts"], 1), Int(ncols/2))
+        for col in 1:2:ncols
+            new_matrix[:, cld(col, 2)] = mean(forecast["forecasts"][:, col:col+1], dims=2)
+        end
+        df = DataFrame(transpose(new_matrix), header)
+    else
+        qunatile_matrix = [parse(Float32, element) for element in forecast["forecasts"]]
+        df = DataFrame(transpose(qunatile_matrix), header)
+    end
+    insertcols!(df, 1, :forecast_time => ["h$i" for i in 0:8759]);
+    return df
+end
+
+df_wind, df_solar, df_load = process_forecast_quantiles(df_wind, df_solar, df_load, df_wind_forecast_quantiles, df_solar_forecast_quantiles, df_load_forecast_quantiles, df_wind_forecast, df_solar_forecast, df_load_forecast)
+# correction DateTimeNY
+df_wind.DateTimeNY = df_wind.DateTime .- Hour(4);
+df_solar.DateTimeNY = df_solar.DateTime .- Hour(4);
+df_load.DateTimeNY = df_load.DateTime .- Hour(4);
+# correcting extracted_hour
+df_wind.extracted_hour = hour.(df_wind.DateTimeNY);
+df_solar.extracted_hour = hour.(df_solar.DateTimeNY);
+df_load.extracted_hour = hour.(df_load.DateTimeNY);
+
+wind_event_quantile = read_event_quantiles(wind_fcst_file)
+solar_event_quantile = read_event_quantiles(solar_fcst_file)
+load_event_quantile = read_event_quantiles(load_fcst_file; isload = true)
+
+
+horizon = 48;
+k = 10; # setting the number of nearest neighbors
+initial_time = Dates.DateTime(2019, 1, 1)
+
+output_dir = "/Users/hanshu/Desktop/Price_formation/Data/generate_fr_KBoot/NYISO/"
+
+for i in 1:(8760-horizon+1)
+    run_time = initial_time + Hour(i - 1)
+    month_of_interest = Dates.month(run_time)
+    day_of_interest = Dates.day(run_time)
+    hour_of_interest = Dates.hour(run_time)
+    wind_plot1, solar_plot1, load_plot1, q_knn1, v_knn1, wind_scenario_blocks_final_variance1, solar_scenario_blocks_final_variance1, load_scenario_blocks_final_variance1 = scenario_generation(df_wind, df_solar, df_load, wind_event_quantile, solar_event_quantile, load_event_quantile, month_of_interest, day_of_interest, horizon, hour_of_interest, k);
+    load_scenarios_array = covert2array(load_scenario_blocks_final_variance1)
+    h5open(output_dir*"load_scenarios.h5", "cw") do file
+        write(file, string(run_time), load_scenarios_array)
+    end
+    solar_scenarios_array = covert2array(solar_scenario_blocks_final_variance1)
+    h5open(output_dir*"solar_scenarios.h5", "cw") do file
+        write(file, string(run_time), solar_scenarios_array)
+    end
+    wind_scenarios_array = covert2array(wind_scenario_blocks_final_variance1)
+    h5open(output_dir*"wind_scenarios.h5", "cw") do file
+        write(file, string(run_time), wind_scenarios_array)
+    end
+end
+
