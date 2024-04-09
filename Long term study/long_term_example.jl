ff126ac5fd00e2c4e0912f1face48fc7b0957080
Siva Visvesvaran <siva18visu@gmail.com>
2024-04-09 15:30:24 -0400

    Cleaned up long_term_study

--- Long term study/long_term_example.jl
+++ Long term study/long_term_example.jl
@@ -38,18 +38,13 @@ wind_quantile = quantile_data_prep(wind_event_quantile)
 solar_quantile = quantile_data_prep(solar_event_quantile)
 load_quantile = quantile_data_prep(load_event_quantile)
 
-insertcols!(wind_quantile, 1, :forecast_time => ["h$i" for i in 0:8759]);
-
 hour_of_interest = 0;
 horizon = 47;
-k = 17; # setting the number of nearest neighbors
+k = 10; # setting the number of nearest neighbors
 month_of_interest = 7;
 day_of_interest = 18;
 
 wind_plot1, solar_plot1, load_plot1, q_knn1, v_knn1, wind_scenario_blocks_final_variance1, solar_scenario_blocks_final_variance1, load_scenario_blocks_final_variance1 = scenario_generation(df_wind, df_solar, df_load, wind_event_quantile, solar_event_quantile, load_event_quantile, month_of_interest, day_of_interest, horizon, hour_of_interest, k);
-
-t = scenario_generation(df_wind, df_solar, df_load, wind_event_quantile, solar_event_quantile, load_event_quantile, month_of_interest, day_of_interest, horizon, hour_of_interest, k)
-
 
 data_to_hdf5("../test/wind_scenario_blocks_hour_$hour_of_interest", wind_scenario_blocks_final_variance1)
 data_to_hdf5("../test/solar_scenario_blocks_hour_$hour_of_interest", solar_scenario_blocks_final_variance1)
